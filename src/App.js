7c141b00f88f0f048e749b0dd8ccd0dc32f6e0d9
SyaibanAhmadRamadhan <ibanrama29@gmail.com>
2023-08-20 04:21:46 +0700

    browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./pages/Home";
-import "./app.css";
-import "./assets/fontawesome/css/all.min.css";
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import './app.css';
+import './assets/fontawesome/css/all.min.css';
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Home />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <HashRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </HashRouter>
+  );
 }
